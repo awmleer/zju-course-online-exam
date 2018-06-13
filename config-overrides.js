4d2d8bea00df8a83ceaa33878e80f1cfa1f4b85c
awmleer <awmleer@sparker.xyz>
2018-06-13 16:33:19 +0800

    fix scss loader

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,8 @@
 const { injectBabelPlugin } = require('react-app-rewired');
+const rewireCssModules = require('react-app-rewire-css-modules');
 
 module.exports = function override(config, env) {
+  config = rewireCssModules(config, env);
   config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
   return config;
 };
