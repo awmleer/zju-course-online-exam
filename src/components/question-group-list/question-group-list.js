0079279d8eedf3320252c18567d3feb153103cff
awmleer <awmleer@sparker.xyz>
2018-06-29 15:59:17 +0800

    use state in component

--- src/components/question-group-list/question-group-list.js
+++ src/components/question-group-list/question-group-list.js
@@ -3,23 +3,43 @@ import {Item, ItemList} from '../item-list/item-list'
 import {Button} from 'antd'
 
 export class QuestionGroupList extends Component {
-  groups = [
-    {id: 1, name: 'test'},
-    {id: 2, name: 'test'},
-    {id: 3, name: 'test'}
-  ];
+
+  constructor(props){
+    super(props)
+    this.state = {
+      groups: [
+        {id: 1, name: 'test'},
+      ]
+    }
+  }
+
+  test = ()=>{
+    this.setState(prevState => ({
+      groups: [
+        {id: 1, name: 'test'},
+        {id: 2, name: 'test'},
+        {id: 3, name: 'test'},
+      ]
+    }))
+  }
+
+
+
   render(){
-    const questionItems = this.groups.map((group) => {
+    const questionItems = this.state.groups.map((group) => {
       return (
         <Item key={group.id.toString()} title={group.name}>
-          <Button type="primary">Primary</Button>
+          <Button type='default' onClick={this.test}>Primary</Button>
         </Item>
       )
     })
     return (
-      <ItemList>
-        {questionItems}
-      </ItemList>
+      <div>
+        <Button type='primary'>创建题库</Button>
+        <ItemList>
+          {questionItems}
+        </ItemList>
+      </div>
     )
   }
 }
