43e260790f790af12fe93aa5a8582b28df72419a
awmleer <awmleer@sparker.xyz>
2018-07-01 14:52:44 +0800

    init exam

---
+++ src/components/exam/exam.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import * as api from '../../api'
+import {withRouter} from 'react-router-dom'
+import {formItemLayoutWithLabel, formItemLayoutWithoutLabel} from '../../form'
+import {Radio, Checkbox, Modal, message, Form, Select, AutoComplete, Button, Input, List, Icon, Tag} from 'antd'
+import './exam.scss'
+
+
+
+export class C extends Component {
+
+}
+
+export const QuestionGroup = withRouter(C)
